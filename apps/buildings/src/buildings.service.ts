9995f0ca86adeafc676fb9f8ade5be6c16b26d9c
MetRyan <101856114+MetRyan@users.noreply.github.com>
2025-02-13 15:57:43 +0700

    setup rabbitmq - Bug

--- apps/buildings/src/buildings.service.ts
+++ apps/buildings/src/buildings.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Injectable()
 export class BuildingsService {
   getHello(): string {
     return 'Hello World!';
   }
+  @MessagePattern({ cmd: 'test' }) // Chỉ định pattern của tin nhắn nhận
+  handleTestMessage(data: string) {
+    console.log('Received message:', data);
+    return `Received: ${data}`; // Trả về phản hồi
+  }
+
+   orders: { email: string }[] = [];
+
+
+
+  getOrders() {
+    return this.orders;
+  }
+
 }
--- apps/buildings/src/main.ts
+++ apps/buildings/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { BuildingsModule } from './buildings.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(BuildingsModule);
-  await app.listen(process.env.port ?? 3000);
+ const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  BuildingsModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+          urls: ['amqp://localhost:5672'], // Địa chỉ RabbitMQ
+          queue: 'buildings_queue', // Tên hàng đợi
+          queueOptions: {
+            durable: false, // Cấu hình để hàng đợi không bền vững (tùy theo yêu cầu)
+          },    
+          },
+    },
+  )
+  await app.listen()
 }
 bootstrap();
