6f4c53d7ff65861f544104aa1a5b956b037bd995
Kane Nguyen <nguyenxuankha5371@gmail.com>
2025-03-19 14:24:41 +0700

    upload image to s3 bucket

--- apps/cracks/src/crack-details/crack-details.module.ts
+++ apps/cracks/src/crack-details/crack-details.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from '../../prisma/prisma.module'; // Import PrismaModule
-import { PrismaService } from '../../prisma/prisma.service';
-import { CrackDetailsController } from './crack-details.controller';
-import { CrackDetailsService } from './crack-details.service'; // Import PrismaService
+import { Module } from '@nestjs/common'
+import { PrismaModule } from '../../prisma/prisma.module' // Import PrismaModule
+import { PrismaService } from '../../prisma/prisma.service'
+import { CrackDetailsController } from './crack-details.controller'
+import { CrackDetailsService } from './crack-details.service' // Import PrismaService
+import { S3UploaderService } from './s3-uploader.service'
 
 @Module({
   imports: [PrismaModule],
   controllers: [CrackDetailsController],
-  providers: [CrackDetailsService, PrismaService],
+  providers: [S3UploaderService, CrackDetailsService, PrismaService],
 })
-export class CrackDetailsModule {}
+export class CrackDetailsModule { }
