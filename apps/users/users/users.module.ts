ebca478b5e4fad8e9148c67b9276008a81f9f4b7
xuankha1011 <nguyenxuankha5371@gmail.com>
2025-03-28 22:32:56 +0700

    fix return of get apartment by resident id

--- apps/users/users/users.module.ts
+++ apps/users/users/users.module.ts
@@ -12,17 +12,22 @@ const BUILDINGS_CLIENT = 'BUILDINGS_CLIENT'
     ClientsModule.registerAsync([
       {
         name: BUILDINGS_CLIENT,
-        useFactory: (configService: ConfigService) => ({
+        useFactory: (configService: ConfigService) => {
+          const user = configService.get('RABBITMQ_USER')
+          const password = configService.get('RABBITMQ_PASSWORD')
+          const host = configService.get('RABBITMQ_HOST')
+          const isLocal = process.env.NODE_ENV !== 'production'
+          return {
           transport: Transport.RMQ,
           options: {
-            urls: ['amqp://admin:admin@rabbitmq:5672'],
+            urls: isLocal ? [`amqp://${user}:${password}@${host}`] : [`amqp://${user}:${password}@rabbitmq:5672`],
             queue: 'buildings_queue',
             queueOptions: {
               durable: true,
               prefetchCount: 1
             }
           }
-        }),
+        }},
         inject: [ConfigService]
       }
     ])
