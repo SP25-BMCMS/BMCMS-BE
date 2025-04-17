52a32373d3e08d54dadf742329b76962d7fa352a
Kane Nguyen <nguyenxuankha5371@gmail.com>
2025-04-17 22:11:25 +0700

    remove --no-engine

--- apps/chatbot/prisma/prisma.service.ts
+++ apps/chatbot/prisma/prisma.service.ts
@@ -1,53 +1,41 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client-chatbot';
-import { withAccelerate } from '@prisma/extension-accelerate';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client-chatbot'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor(private configService: ConfigService) {
-    // const accelerateUrl = configService.get<string>('CHATBOT_PRISMA_ACCELERATE_URL');
-    // if (!accelerateUrl) {
-    //   throw new Error('CHATBOT_PRISMA_ACCELERATE_URL is not defined');
-    // }
-    const url = configService.get<string>('DB_CHATBOT_SERVICE')
+    const url = configService.get<string>('DB_CHATBOT_URL')
+    if (!url) {
+      throw new Error('DB_CHATBOT_URL is not defined')
+    }
 
     super({
-      log: ['error', 'warn'],
       datasources: {
         db: {
-          // url: accelerateUrl
           url: url
         }
-      }
-    });
+      },
+      log: ['error', 'warn']
+    })
   }
 
   async onModuleInit() {
     try {
-      // Pre-warm Prisma Client
-      await Promise.all([
-        this.$connect(),
-        this.$queryRaw`SELECT 1` // Simple query to warm up connection
-      ]);
-      
-      // Apply Accelerate extension
-      // this.$extends(withAccelerate());
-      // console.log('Successfully connected to database with Accelerate');
+      await this.$connect()
+      await this.$queryRaw`SELECT 1`
     } catch (error) {
-      console.error('Failed to connect to database:', error);
-      throw error;
+      console.error('Failed to connect to database:', error)
+      throw error
     }
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.$disconnect()
   }
 
   async cleanDatabase() {
-    if (process.env.NODE_ENV === 'production') return;
-
-    // teardown logic
-    return Promise.all([this.chat.deleteMany()]);
+    if (process.env.NODE_ENV === 'production') return
+    return Promise.all([this.chat.deleteMany()])
   }
 } 
