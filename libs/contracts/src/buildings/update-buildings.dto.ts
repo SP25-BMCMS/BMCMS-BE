b9725ce3f7746c50e67e7ff80dab3ca1bd1cc736
MetRyan <101856114+MetRyan@users.noreply.github.com>
2025-04-25 12:14:09 +0700

    update building

--- libs/contracts/src/buildings/update-buildings.dto.ts
+++ libs/contracts/src/buildings/update-buildings.dto.ts
@@ -4,13 +4,13 @@ import { IsString, IsInt, IsOptional, IsUUID, IsNotEmpty, IsEnum } from 'class-v
 
 export class UpdateBuildingDto {
 
-  @ApiProperty({
-    description: 'Unique identifier for the building',
-    type: String,
-    example: 'd1b0cd4c-1e76-4d7f-a0d4-81b32e5101cd', // Example UUID
-  })
-  @IsUUID()
-  buildingId: string;
+  // @ApiProperty({
+  //   description: 'Unique identifier for the building',
+  //   type: String,
+  //   example: 'd1b0cd4c-1e76-4d7f-a0d4-81b32e5101cd', // Example UUID
+  // })
+  // @IsUUID()
+  // buildingId: string;
 
   @ApiProperty({
     description: 'Name of the building (optional)',
